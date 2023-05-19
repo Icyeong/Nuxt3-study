da158e81b4a8de51a1a485006603cdbfd7ebc00b
chaeyeong Imm <cyim@chonf.co.kr>
2023-05-19 14:37:26 +0900

    .

--- test.ts
+++ test.ts
@@ -1,16 +1,1 @@
-let a: number = 3;
-a = 25;
-
-let b: any = 23;
-b = "hello";
-b = null;
-let d: string | number = "hihi";
-d = 33;
-
-let e: string[] = ["a", "b", "c"];
-
-function add(f: number, g: number): number {
-  return a + b;
-}
-
-add(3, 5);
+// https://www.youtube.com/watch?v=ww94Jvi8JJo&list=PL8HkCX2C5h0XT3xWYn71TlsAAo0kizmVc&index=21&t=9438s
